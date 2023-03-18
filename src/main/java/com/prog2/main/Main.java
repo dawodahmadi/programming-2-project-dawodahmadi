77db5d2d85a4c0a47839b01cc7a49c1d5deaa03c
dawodahmadi <ahmadi.dawod@gmail.com>
2023-03-18 12:40:00 -0400

    first commit here

---
+++ src/main/java/com/prog2/main/Main.java
@@ -0,0 +1,18 @@
+package com.prog2.main;
+/**
+ * @author adinashby
+ *
+ */
+
+public class Main {
+
+	/**
+	 * Write your test code below in the main.
+	 *
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+	
+}
